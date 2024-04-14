f61b96194dc2a92155459e1e2077a2284d12c52f
LoraBn <77924067+LoraBn@users.noreply.github.com>
2024-04-14 04:23:28 +0300

    Update database connection string to include SSL parameter
    The connection string for the PostgreSQL database has been updated to include the `ssl=true` parameter. This change was made to ensure a secure connection to the database.

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?ssl=true`;
 
 const { Pool } = require('pg');
 const pool = new Pool({
